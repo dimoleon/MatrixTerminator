a413beca4ddb536c29c6666dcdee9a4b2fef241b
ThanasisGeo <96844738+ThanasisGeo@users.noreply.github.com>
2022-01-04 19:53:33 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 int
 main (void) 
 {
@@ -9,23 +8,26 @@ printf ("What do you want the program to do? (Type 0 for Options)");
 int i;
 
 there:
-scanf ("%d", &i);
+i = getchar();
   
 if (i == 0)
     
     {
       printf
 	("The program can do the following:\n-Create Matrix(1)\n-Load Matrix(2)\n-Edit Matrix(3)\n-View Existing Matrix(4)\n-Write(5)\n-Do Operations with Matrices(6)\n\n Just press the code of the command you want to use!");
-      
+fflush(stdin);      
 goto there;
       
 if (i == 1)		// Create Matrix.
-	{printf("-Create new Matrix(1)\n-Copy other Matrix.(2)\n-Pull values from other matrix in the form of rows and columns.(3)");
-scanf ("%d", &i);
+here:	
+{printf("-Create new Matrix(1)\n-Copy other Matrix.(2)\n-Pull values from other matrix in the form of rows and columns.(3)");
+fflush(stdin);
+i = getchar();
 if (i == 1){}
 if (i == 2){}
 if (i == 3){}
-	
+if( i<1 || i>3){printf("Wrong input! Try again..");
+goto here;
 }
       
 if (i == 2)		// Load Matrix.
@@ -37,19 +39,25 @@ if (i == 3)		// Edit Matrix.
 	}
       
 if (i == 4)		// View Existing Matrices.
-	{printf("Do you want to show all matrices (0) or single? (Ίσως για κωδικό το id?)");
-scanf("%d", &i);
-if(i =0){}
-if(i == id){}
+	{printf("Do you want to show all matrices (0) or a single?(1)");
+fflush(stdin);
+i = getchar();
+if(i == 0){}
+if(i == 1){}
 	}
 
 if (i == 5){}           // Write.        
 if (i == 6)		// Do Operations with Matrices.
+if(i<1 || i>6)
+{printf("Wrong input! Try again..");
+goto there;}
 {
+oper:
 	  printf
 	    ("These are the operation codes:-Addition(1).\n-Subtraction(2).\n-Multiplication(3).\n- Transposition(4).\n- Scalar Multiplication(5). \n-Determinant(6).\n-Row Echelon from(7)\n\n ~The following are for vectors only~\n\n-Dot product(8)\n-Cross product(9)\n-(Mixed) product(10).\n");
 	  
-scanf ("%d", &i);
+fflush(stdin);
+i = getchar();
 	  
 if (i == 1)		// Addition.
 	    {
@@ -87,14 +95,13 @@ if (i == 9)		// Cross product.
 if (i == 10)		// (Mixed) product.
 	    {
 	    }	
+if(i > 10 || i < 1)
+{printf("Wrong Input! Try again..");
+goto oper;
 }
       
 return 0;
     
 }
   
- 
- 
- 
- 
  
